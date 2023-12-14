c8acce624c361053dd6906eecfc4615f7b36babf
nsoftnhdev <n.softnhdev@gmail.com>
2023-12-14 10:54:45 +0800

    Make delivery option interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -107,7 +107,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
   cartItem.deliveryOptionId;
 
     html += `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
       <input type="radio"
         ${isChecked ? 'checked' : ''}
         class="delivery-option-input"
@@ -133,11 +135,18 @@ document.querySelector('.js-order-summary')
 document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
-      const producId = link.dataset.productId;
-      removeFromCart(producId);
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
 
-      const container = document.querySelector(`.js-cart-item-container-${producId}`);
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
     });
   });
+
+  document.querySelectorAll('.js-delivery-option').forEach((element) => {
+      element.addEventListener('click', () => {
+        const {productId, deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId, deliveryOptionId);
+      });
+    });
 
